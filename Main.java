6d535e06043a0d32b97349729819fef84ec10735
Karneeshwar <78437398+karneeshwar@users.noreply.github.com>
2021-10-20 13:22:42 -0500

    Add files via upload
    Heapify and Heapsort

--- Main.java
+++ Main.java
@@ -1,109 +1,92 @@
-//Program to perform insertion, inorder traversal and deletion in a BST
-//Author:
-    //Name: Karneeshwar, Sendilkumar Vijaya
-
-//Class defined for the Tree
-class Node
-{
-    int data;
-    Node left;
-    Node right;
-
-    Node(int element)
-    {
-        data = element;
-        left = null;
-        right = null;
-    }
-}
+/*
+Program to build a minheap using Floyd's algorithm and sort it in descending order
+Author
+Name	: Karneeshwar, Sendilkumar Vijaya
+NetID	: KXS200001 
+*/
 
 public class Main
 {
-    //Function to insert elements into the tree recursively
-    public static Node insertElement(Node root, int element)
+    //Function to make an array into a minheap using Floyd's algorithm
+    static void minHeap(int[] arr, int parent, int l)
     {
-        if(root == null)
-            return new Node(element);
+        int tmpPos = parent;  							//Variable to track the position of the smallest value, assuming its in current parent initially
+        int lChild = 2*parent + 1; 						//Position of left child of the current parent
+        int rChild = 2*parent + 2; 						//Position of right child of the current parent
 
-        if(element <= root.data)
-            root.left = insertElement(root.left, element);
-        else
-            root.right = insertElement(root.right,element);
+        if (lChild < l && arr[lChild] < arr[tmpPos]) 	//If left child exists and if it's smaller than the one at current position
+            tmpPos = lChild; 							//Then new position of the smallest node is the position of left child
 
-        return root;
-    }
+        if (rChild < l && arr[rChild] < arr[tmpPos]) 	//If right child exists and if it's smaller than the one at current position
+            tmpPos = rChild; 							//Then new position of the smallest node is the position of right child
 
-    //Function to perform inorder traversal of the BST
-    public static void inorderTraversal(Node root)
-    {
-        if(root == null)
-            return;
-
-        inorderTraversal(root.left);
-        System.out.print(root.data + " ");
-        inorderTraversal(root.right);
-    }
-
-    //Function to find the successor to replace the item to be deleted
-    public static Node findSuccessor(Node root)
-    {
-        Node successor = root.right;
-        while(successor != null && successor.left != null)
-            successor = successor.left;
-        return successor;
-    }
-
-    //Function to delete an element from the BST and replace it with successor
-    public static Node deleteElement(Node root, int element)
-    {
-        if(root == null)
-            return null;
-
-        if(root.data > element)
-            root.left = deleteElement(root.left, element);
-
-        else if(root.data < element)
-            root.right = deleteElement(root.right, element);
-
-        else
+        if (tmpPos != parent) 							//If the current position is not same as parent node's position
         {
-            if(root.left == null)
-                return root.right;
-            else if (root.right == null)
-                return root.left;
-            else 
-			{
-                Node inorderSuccessor = findSuccessor(root);
-                root.data = inorderSuccessor.data;
-                root.right = deleteElement(root.right, inorderSuccessor.data);
-            }
+            int tmp = arr[parent]; 						//Then swap the smallest one with the parent
+            arr[parent] = arr[tmpPos];
+            arr[tmpPos] = tmp;
+            minHeap(arr, tmpPos, l); 					//Recursively call minHeap to check the sub-tress as well
         }
-        return root;
     }
 
+    //Function to perform heapsort on the array
+    static void heapSort(int[] arr, int len)
+    {
+        for(int i = len-1; i>0; i--) 					//Reducing the length of the array at each iteration to complete sorting
+        {
+            int tmp = arr[0]; 							//Swapping the root and last leaf node elements
+            arr[0] = arr[i];
+            arr[i] = tmp;
+
+            minHeap(arr,0, i); 							//Calling function to convert the new array to a heap
+        }
+    }
+
+    //Function to find the last parent and iterate the heap building process to subtrees
+    static void buildMinHeap(int[] arr, int len)
+    {
+        int lastParent = len/2 - 1; 					//last parent of the heap
+
+        for (int i = lastParent; i >= 0; i--) 			//Moving towards root by iterating through each parent
+            minHeap(arr, i, len);
+    }
+
+    //Function to print the elements of the heap stored as an array
+    static void printHeapAsArray(int[] arr, int len)
+    {
+        for(int i = 0; i < len; i++)
+            System.out.print(arr[i] + " ");
+        System.out.print("\n");
+    }
+
+    //Main function
     public static void main(String[] args)
     {
-        System.out.print("\nCS5343.002 Assignment 2: Insertion, Inorder traversal and Deletion in BST\n");
+        System.out.print("\nCS5343.002 Assignment 3: Building and Sorting Heap\n");
 
-        //Initializing Arrays of elements to be added in the BST
-        int[] elements = new int[]{40, 60, 20, 80, 50, 10, 30, 15, 5, 35, 25, 45, 55, 70, 90, 32, 33, 48, 46};
-        Node root = null;
+        //Initializing Arrays of elements to be added in the heap
+        int[] array = {40, 60, 20, 80, 50, 10, 30, 14, 65, 35, 25, 45, 55, 32, 16};
+        int length = array.length;
 
-        //Calling function to insert element to the BST
-        for(int i: elements)
-            root = insertElement(root, i);
+        //Printing initial array before building heap
+        System.out.print("\nThe number of elements in the heap = " + length);
+        System.out.print("\nSequence of elements before building the heap: \n");
+        printHeapAsArray(array, length);
 
-        System.out.print("\nInitially the BST contains (Inorder Traversal):\n\t");
-        inorderTraversal(root);
+        //Building heap
+        buildMinHeap(array, length);
+        //Printing array after building the heap
+        System.out.print("\nThe number of elements in the heap = " + length);
+        System.out.print("\nSequence of elements after building the heap: \n");
+        printHeapAsArray(array, length);
 
-        root = deleteElement(root, 40); //Deleting element 40 from the BST
-        System.out.print("\n\nThe BST after deleting 40 (Inorder Traversal):\n\t");
-        inorderTraversal(root);
-
-        root = deleteElement(root, 20); //Deleting element 20 from the BST
-        System.out.print("\n\nThe BST after deleting 20 (Inorder Traversal):\n\t");
-        inorderTraversal(root);
-
-        System.out.print("\n\nEnd of Results!!\n");
+        //Heapsort
+        heapSort(array, length);
+        //Printing the array after sorting the heap
+        System.out.print("\nThe number of elements in the heap = " + length);
+        System.out.print("\nSequence of elements after sorting the heap: \n");
+        printHeapAsArray(array, length);
+		
+		System.out.print("\nEnd of Results!!\n\n");
     }
 }
